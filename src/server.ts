98821b39ed83923ce71f1ee1c1521f87c7f7b25e
Salauddin-Ahmad <salauddinahmed555@gmail.com>
2025-12-04 20:43:22 +0600

    get users and also a single users

--- src/server.ts
+++ src/server.ts
@@ -1,67 +1,3 @@
-// import express, { Request, Response } from "express";
-
-// import { Pool } from "pg";
-
-// const app = express();
-// const port = 5000;
-
-// // DB
-// const pool = new Pool({
-//   connectionString: `postgresql://neondb_owner:npg_Ila1BLG4CpFq@ep-restless-base-afctc6th-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
-// });
-
-// app.use(express.json());
-// app.use(express.urlencoded());
-
-// const initDB = async () => {
-//   await pool.query(`
-//         CREATE TABLE IF NOT EXISTS users(
-//         id SERIAL PRIMARY KEY,
-//         name VARCHAR(100) NOT NULL,
-//         email VARCHAR(150) UNIQUE NOT NULL,
-//         age INT,
-//         phone VARCHAR(15),
-//         address TEXT,
-//         created_at TIMESTAMP DEFAULT NOW(),
-//         updated_at TIMESTAMP DEFAULT NOW()
-//         )
-//         `);
-
-//   await pool.query(`
-//             CREATE TABLE IF NOT EXISTS todos(
-//             id SERIAL PRIMARY KEY,
-//             user_id INT REFERENCES users(id) ON DELETE CASCADE,
-//             title VARCHAR(200) NOT NULL,
-//             description TEXT,
-//             completed BOOLEAN DEFAULT false,
-//             due_date DATE,
-//             created_at TIMESTAMP DEFAULT NOW(),
-//             updated_at TIMESTAMP DEFAULT NOW()
-//             )
-//             `);
-// };
-
-// initDB().then(() => {
-//   app.listen(port, () => console.log(`DATABASE Server running`));
-// });
-
-// app.get("/", (req: Request, res: Response) => {
-//   res.send("Hello World!");
-// });
-
-// app.post("/p", (req: Request, res: Response) => {
-//   console.log(req.body);
-
-//   res.status(201).json({
-//     success: true,
-//     message: "API is working",
-//   });
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
-
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
 
@@ -95,7 +31,7 @@ const initDB = async () => {
     `);
 
     await pool.query(`
-    CREATE TABLE IF NOT EXISTS todos (
+    CREATE TABLE IF NOT EXISTS todos(
         id SERIAL PRIMARY KEY,
         user_id INT REFERENCES users(id) ON DELETE CASCADE,
         title VARCHAR(200) NOT NULL,
@@ -117,4 +53,78 @@ initDB().then(() => {
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
+      [name, email]
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "data inserted",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * from users`);
+    res.status(200).json({
+      success: true,
+      message: "users retrieved successfully",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      details: error,
+    });
+  }
+});
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    console.log(req.params.id);
+
+    const result = await pool.query(`SELECT * from users WHERE id = $1`, [
+      req.params.id,
+    ]);
+    console.log(result);
+    // res.status(200).json({
+    //   success: true,
+    //   message: "users retrieved successfully",
+    //   data: result.rows,
+    // });
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "users not found",
+      });
+    }
+    else {
+        res.status(200).json({
+        success: true,
+        message: "user fetched successfully",
+        data: result.rows[0]
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      details: error,
+    });
+  }
 });
